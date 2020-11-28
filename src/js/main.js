700496c7f1c4097b5d5861cbbcd0c3c544100344
Daniil Khryapin <dkhryapin@inbox.ru>
2020-11-28 17:52:05 +0300

    задание 7

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,33 @@
-let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда','Четверг','Пятница','Суббота'];
+let week = ['Понедельник', 'Вторник', 'Среда','Четверг','Пятница','Суббота','Воскресенье'];
+let dateArr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда','Четверг','Пятница','Суббота']; // заведём массив Date
 let date = new Date;
+let today = date.getDay(); //запишем сегодняшний день
 const ul = document.querySelector('ul');
-week.forEach((item, i, week) => {
-    if(i === date.getDay()) {
-        createLi(item, 'bold');
+week.forEach((item, i) => {
+    createLi(item);
+    if(item === dateArr[today]) { // для нахождения сегодняшнего дня в нашей неделе мы перебором найдём в нашей неделе тот день недели который является today
+        custom(item, 'bold');
     }
-    else if(i > 4) {
-        createLi(item, 'italic');
-    }else {
-        createLi(item);
+    if(i > 4){
+        custom(item, 'italic');
     }
+    delCustom(item); // зачистка промежуточных классов
 })
 
-function createLi(item, custom) {
+function createLi(item) { // создание элемента списка
     const li = document.createElement('li');
     li.textContent = item;
-    li.classList.add(custom);
+    li.classList.add(item); // добавим промежуточный класс для того чтобы в функции custom найти нужный нам элемент и записать в него нужный нам класс
     ul.appendChild(li);
 }
+
+function custom(item, value) { // функция добавления класса
+    let element = document.querySelector(`.${item}`); 
+    element.classList.add(value);
+}
+
+function delCustom(item) { // функция зачистки промежуточных классов
+    let element = document.querySelector(`.${item}`);
+    element.classList.remove(item);
+}
+
