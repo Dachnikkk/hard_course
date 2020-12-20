660aa2a3493f01dd316a8b7b3a00ce829a407108
Daniil Khryapin <dkhryapin@inbox.ru>
2020-12-20 03:59:44 +0300

    lesson_14

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,46 @@
-const btn = document.querySelector('#change');
-const color = document.querySelector('#color');
-const body = document.querySelector('body')
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    let body = document.querySelector('body');
 
-btn.addEventListener('click', newColor);
+    const DomElement = function(selector, height, width, bg, position) {
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.position = position;
+    }
 
-function newColor() {
-    let random = Math.floor(Math.random() * 10000000).toString(16);
-    color.textContent = '#' + random;
-    body.style.background = '#' + random;
-}
+    DomElement.prototype.newElement = function() {
+        let div = document.createElement('div');
+        let newId = this.selector.slice(1);
+        div.setAttribute('id', newId);
+        div.style.height = this.height;
+        div.style.width = this.width;
+        div.style.background = this.bg;
+        div.style.position = this.position;
+
+        body.append(div);
+    };
+    const domElementId = new DomElement('#block', '100px', '100px', 'green', 'absolute');
+    domElementId.newElement();
+    
+    const elem = document.querySelector('#block');
+    let countTopBottom = 0,
+        countLeftRight = 0;
+
+    document.addEventListener('keydown', function(event) {
+        if(event.key === 'ArrowRight') {
+            countLeftRight += 10;
+            elem.style.left = countLeftRight + 'px';
+        }else if(event.key === 'ArrowLeft') {
+            countLeftRight -= 10;
+            elem.style.left = countLeftRight + 'px';
+        }else if(event.key === 'ArrowDown') {
+            countTopBottom += 10;
+            elem.style.top = countTopBottom + 'px';
+        }else if(event.key === 'ArrowUp') {
+            countTopBottom -= 10;
+            elem.style.top = countTopBottom + 'px';
+        }
+    });
+});
